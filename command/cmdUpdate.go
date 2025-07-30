38bae1f82e0769f4d366e58c68ae8b3a54154b09
David Krutsko <dave@krutsko.net>
2025-07-30 15:32:05 -0400

    Revert "Add a required keys tag to force the update to wait until the retry time is hit, or the required keys are made available. (#10)"
    This reverts commit 990d117e611814db79a703c6889f4988f6e26030.

--- command/cmdUpdate.go
+++ command/cmdUpdate.go
@@ -2,7 +2,6 @@ package command
 
 import (
 	"flag"
-	"strings"
 	"time"
 
 	"github.com/StackAdapt/systags/manager"
@@ -10,22 +9,7 @@ import (
 
 type UpdateCommand struct {
 	baseCommand
-	timeout     time.Duration
-	retry       time.Duration
-	requredKeys StringArray
-}
-
-type StringArray []string
-
-// String is the method to format the flag's value, part of the flag.Value interface.
-func (s *StringArray) String() string {
-	return strings.Join(*s, ",")
-}
-
-// Set is the method to set the flag value, part of the flag.Value interface.
-func (s *StringArray) Set(value string) error {
-	*s = strings.Split(value, ",")
-	return nil
+	timeout time.Duration
 }
 
 func NewUpdateCommand() *UpdateCommand {
@@ -38,10 +22,6 @@ func NewUpdateCommand() *UpdateCommand {
 
 	cmd.flagSet.DurationVar(&cmd.timeout, "t", 5*time.Second, "")
 	cmd.flagSet.DurationVar(&cmd.timeout, "timeout", 5*time.Second, "")
-	cmd.flagSet.DurationVar(&cmd.retry, "r", 0*time.Second, "")
-	cmd.flagSet.DurationVar(&cmd.retry, "retry", 0*time.Second, "")
-	cmd.flagSet.Var(&cmd.requredKeys, "k", "")
-	cmd.flagSet.Var(&cmd.requredKeys, "requried_keys", "")
 
 	// Don't print unneeded usage
 	cmd.flagSet.Usage = func() {}
@@ -56,7 +36,7 @@ func (cmd *UpdateCommand) Apply(m *manager.Manager) error {
 		return err
 	}
 
-	err = m.UpdateRemote(cmd.timeout, cmd.retry, cmd.requredKeys)
+	err = m.UpdateRemote(cmd.timeout)
 	if err != nil {
 		return err
 	}
